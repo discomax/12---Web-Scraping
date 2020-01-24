55533198ccea3e63a38ca5c3362b5078f3fc3de7
Mike Patterson <mpatterson73@gmail.com>
2020-01-23 21:48:15 -0500

    added the driver and conn string for Mongo Atlas

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import scrape_mars
 
 app = Flask(__name__)
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+# driver with connection string for use with MongoDB Atlas AWS cloud service:
+# client = pymongo.MongoClient("mongodb+srv://disco_max:<password>@cluster0-ey2fy.mongodb.net/test?retryWrites=true&w=majority")
+# db = client.test
 
 
 @app.route("/")
